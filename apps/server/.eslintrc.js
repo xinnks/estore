d125b968903f459919cb5c7ce5a3894b5955c63d
James Sinkala <xinnks@gmail.com>
2022-08-07 04:03:58 +0300

    feat: Add server app

---
+++ apps/server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
---
+++ apps/server/prisma/seed.js
@@ -0,0 +1,111 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const categoryData = [
+  {
+    name: "Accessories",
+    articles: {
+      create: [
+        {
+          name: "Butler - Tortoise frame with Rust Mirror ",
+          price: "35.00",
+          image: "https://asset-train.twic.pics/images/shades-1.jpg",
+          details:
+            "The Butler is our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+        {
+          name: "Slate Sunglassse",
+          price: "30.00",
+          image: "https://asset-train.twic.pics/images/shades-2.jpg",
+          details:
+            "The Slate Sunglasses are our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+        {
+          name: "Super Bass Headphones",
+          price: "60.00",
+          image: "https://asset-train.twic.pics/images/headphone-1.jpg",
+          details:
+            "The Super Bass Headphones is our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+      ],
+    },
+  },
+  {
+    name: "Shoes",
+    articles: {
+      create: [
+        {
+          name: "Baby Shoes",
+          price: "22.00",
+          image: "https://asset-train.twic.pics/images/shoes-1.jpg",
+          details:
+            "The Baby Shoes are our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+        {
+          name: "Super MVP Sneakers",
+          price: "87.00",
+          image: "https://asset-train.twic.pics/images/shoes-2.jpg",
+          details:
+            "The Super MVP Sneakers are our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+        {
+          name: "Hip Kicks",
+          price: "38.00",
+          image: "https://asset-train.twic.pics/images/shoes-3.jpg",
+          details:
+            "The Hip Kicks are our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+      ],
+    },
+  },
+  {
+    name: "Furniture",
+    articles: {
+      create: [
+        {
+          name: "Brown Furry Leather",
+          price: "120.00",
+          image: "https://asset-train.twic.pics/images/chair-1.jpg",
+          details:
+            "The Brown Furry Leather is our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+        {
+          name: "Wooden Relax",
+          price: "180.00",
+          image: "https://asset-train.twic.pics/images/chair-2.jpg",
+          details:
+            "The Wooden Relax is our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+        {
+          name: "Light Leather Sofa",
+          price: "180.00",
+          image: "https://asset-train.twic.pics/images/sofa-1.jpg",
+          details:
+            "The Light Leather Sofa is our restyled and redesigned classic. It's an ultra-reliable choice for all with it's medium size and timeless wire accents.",
+        },
+      ],
+    },
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const cat of categoryData) {
+    const category = await prisma.categories.create({
+      data: cat,
+    });
+    console.log(`Created category with id: ${category.id}`);
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
