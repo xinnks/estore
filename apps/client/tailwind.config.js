6509d7335b34946cf522a4de53e284808cff058e
James Sinkala <xinnks@gmail.com>
2022-08-11 16:57:23 +0300

    style: Add and configure tailwindcss

---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
