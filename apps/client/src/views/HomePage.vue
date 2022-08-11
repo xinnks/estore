36f5a4e5991cefc1cdd1e220d9ca3f28ff6cdb94
James Sinkala <xinnks@gmail.com>
2022-08-11 17:12:20 +0300

    [client] Add product pages and routing  (#6)
    * feat: Add home page
    
    * feat: Add product category page
    
    * feat: Add product details page
    
    * feat: Add 404 page
    
    * feat: Add navbar
    
    * feat: Add page routing
    
    * style: Add and configure tailwindcss
    
    * asset: Add estore's logo asset

---
+++ apps/client/src/views/HomePage.vue
@@ -0,0 +1,53 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+let products = ref([]);
+
+const fetchAllProducts = async () => {
+  try {
+    const { data } = await axios.get("http://localhost:5000/articles");
+    products.value = data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+fetchAllProducts();
+</script>
+
+<template>
+  <div class="bg-white">
+    <div
+      class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
+    >
+      <h2 class="sr-only">Products</h2>
+
+      <div
+        class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
+      >
+        <router-link
+          v-for="product in products"
+          :key="product.id"
+          :to="`/product/${product.id}`"
+          class="group"
+        >
+          <div
+            class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8"
+          >
+            <img
+              :src="product.image"
+              :alt="product.name"
+              class="w-full h-full object-center object-cover group-hover:opacity-75"
+            />
+          </div>
+          <h3 class="mt-4 text-sm text-gray-700">
+            {{ product.name }}
+          </h3>
+          <p class="mt-1 text-lg font-medium text-gray-900">
+            ${{ product.price }}
+          </p>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ apps/client/src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen w-full bg-gray-50 flex items-center">
+    <div
+      class="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700"
+    >
+      <div class="w-full lg:w-1/2 mx-24 text-center md:text-left">
+        <p class="text-3xl md:text-4xl font-light leading-normal mb-8">
+          Sorry we couldn't find the page you're looking for
+        </p>
+
+        <router-link
+          to="/"
+          class="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-cyan-600 active:bg-cyan-700 hover:bg-cyan-700 hover:text-white"
+          >back to homepage</router-link
+        >
+      </div>
+      <div class="w-full lg:flex lg:justify-end lg:w-1/2 mx-5 my-12">
+        <img src="../assets/404.svg" class="" alt="Page not found" />
+      </div>
+    </div>
+  </div>
+</template>
